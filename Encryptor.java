d6fa931c09b4af3027920e2c28d4db6708e99088
Francisco Rogel <franciscorogel4@gmail.com>
2017-12-01 20:53:03 -0500

    Add files via upload

---
+++ Encryptor.java
@@ -0,0 +1,17 @@
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+public class Encryptor {
+
+	public static void main(String[] args) {
+
+		Random randGenerator = new Random();
+		
+		for (int i = 0; i < 100; i++){
+			
+		}
+		
+	}
+
+}
